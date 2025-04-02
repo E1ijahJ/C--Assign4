534236c85a057293765320a071a93e6e391b209d
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-01 23:31:36 -0400

    Added documentation for all said functions

--- src/SystemFunctions.cpp
+++ src/SystemFunctions.cpp
@@ -1,7 +1,9 @@
 #include <cstdlib>  
 #include <string>
 
-
+/// @brief Creates a number of directoreis using built in mkdir command
+/// @param count number of directoreis to be created
+/// @param baseName the base name for each directory 
 void createDirectories(int count, const std::string& baseName) {
     for (int i = 0; i < count; ++i) {
         std::string command = "mkdir " + baseName + std::to_string(i);
@@ -9,13 +11,14 @@ void createDirectories(int count, const std::string& baseName) {
     }
 }
 
-
+/// @brief calls the echo command to print inputed string to the terminal
+/// @param message the inputed string
 void echoString(const std::string& message) {
     std::string command = "echo \"" + message + "\"";
     system(command.c_str());
 }
 
-
+/// @brief This initalizes a git repo, adds to the repo then commits.
 void initializeGitRepo() {
     system("git init");
     system("git add .");
